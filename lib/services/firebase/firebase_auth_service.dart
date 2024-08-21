90afd97ce203e091d4038ba0bb8a5437f13ecb99
Kamran Khan <kamrankhan.sde@gmail.com>
2024-08-21 18:29:30 +0530

    Issue Solved

--- lib/services/firebase/firebase_auth_service.dart
+++ lib/services/firebase/firebase_auth_service.dart
@@ -1,5 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
