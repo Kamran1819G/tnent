43b64a79e730ad76eaeb7b2ddb325f93ed865fc4
ti8anACEx <jdn3210@gmail.com>
2024-09-22 10:36:07 +0530

    1.0.9+9 google login removed

--- lib/presentation/pages/signin_screen.dart
+++ lib/presentation/pages/signin_screen.dart
@@ -251,6 +251,7 @@ class _SignInScreenState extends State<SignInScreen> {
                         )),
               SizedBox(height: 160.h),
               // Create google and apple sign in buttons
+              /*
               Center(
                 child: Text(
                   'Or Sign In With',
@@ -349,7 +350,8 @@ class _SignInScreenState extends State<SignInScreen> {
                   ),
                 ),
               ),
-              SizedBox(height: 30.h),
+              */
+              // SizedBox(height: 30.h),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
